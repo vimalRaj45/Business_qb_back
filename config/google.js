f50184f682f752cb84651f3907de3ec80b1ea1f4
vimalRaj45 <vimalr@spirelia.com>
2026-09-09 00:27:35 +0530

    chore: narrow scopes to drive.file

--- config/google.js
+++ config/google.js
@@ -4,7 +4,6 @@ import { env } from './env.js';
 export const GOOGLE_SCOPES = [
   'https://www.googleapis.com/auth/userinfo.profile',
   'https://www.googleapis.com/auth/userinfo.email',
-  'https://www.googleapis.com/auth/spreadsheets',
   'https://www.googleapis.com/auth/drive.file',
   'https://www.googleapis.com/auth/gmail.send'
 ];
